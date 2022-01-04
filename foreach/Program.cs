9d92c329588ab1558fb001d14db46ef6de3201f1
303DILARA_AKSAM <303DILARA_AKSAM@DESKTOP-HNE43R2>
2022-01-04 15:14:04 +0300

    Proje dosyası ekle.

---
+++ foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // foreach ile dizideki elemanları yazdırma
+            double[] ondaliklisayilar = { 1.2, 2.3, 3.4, 4.5, 5.6, 6.7 };
+            foreach (double c in ondaliklisayilar)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ foreach_ornek2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach_ornek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //foreach ile mevsimleri yazdırma
+            string[] mevsimler = { "İlkbahar", "Yaz","Sonbahar","Kış" };
+            foreach (string b in mevsimler)
+            {
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
